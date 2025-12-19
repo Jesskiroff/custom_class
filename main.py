cd7ecb4ba6e08519842dfa8969b20802a06d0677
Jessica Kiroff <jesskiroff@gmail.com>
2025-12-18 21:15:52 -0500

    add cunstructor to class object

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+# class User:
+#      pass
+
+
+# user_1 = User()
+# user_1.id = "001"
+# user_1.username = "Alice" # Assigning username attribute (an attribute is attached to a variable using dot notation)
+
+# print(user_1.username)
+
 class User:
-     pass
-
-
-
-user_1 = User()
-user_1.id = "001"
-user_1.username = "Alice"
-
-print(user_1.username)
+    def __init__(self, user_id, username):  # __init__ is a special method called a constructor
+        self.id = user_id                  # Attributes are defined within the constructor
+        self.username = username
